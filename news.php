58e7056911828763398f144bce4700ee12f18f47
ptflp <globallinkliberty@yandex.ru>
2017-01-15 22:55:15 +0900

    news added

---
+++ news.php
@@ -0,0 +1,35 @@
+<?
+include 'header.php'; //хидер
+?>
+		<!--=== Content Part ===-->
+		<div class="container content">
+			<div class="row">
+				<!-- Begin Content -->
+				<div class="col-md-9">
+					<ul class="timeline-v2">
+						<li class="equal-height-columns">
+							<div class="cbp_tmtime equal-height-column"><span>4/1/17</span> <span>Январь</span></div>
+							<i class="cbp_tmicon rounded-x hidden-xs"></i>
+							<div class="cbp_tmlabel equal-height-column">
+								<h2>Первая новость</h2>
+								<div class="row">
+									<div class="col-md-4">
+										<img class="img-responsive" src="assets/img/blog/29.jpg" alt="">
+										<div class="md-margin-bottom-20"></div>
+									</div>
+									<div class="col-md-8">
+										<p>Велосипед — двухколесное средство передвижения, движение которого осуществляется за счет верчения педалей ногами. За долгую историю своего существования велосипед претерпел множество изменений и повидал многие виды — теперь у нас есть и горный велосипед, и цирковой, и кроссовый, и для выполнения акробатических трюков. Если прикрутить к велосипеду мотор, получится мопед. Стоимость велосипеда растет соразмерно количеству примочек, качеству материала и надежности транспортного средства. Во всем мире есть множество любителей этого неприхотливого и крайне полезного средства передвижения, а в некоторых странах велосипедов больше, наверное, чем людей (хотя это, скорее всего, голландский миф).</p>
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<!-- End Content -->
+			</div>
+		</div><!--/container-->
+		<!--=== End Content Part ===-->
+
+<?
+include 'footer.php'; //футер
+?>
